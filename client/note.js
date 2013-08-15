99c624ed2320d70165e889b271521135057dee20
nicolas philippe <nicolas.philippe33@gmail.com>
2013-08-15 16:53:07 +0200

    Ajout de la gestion des notes

---
+++ client/note.js
@@ -0,0 +1,37 @@
+function saveNewNote(e, templ) {
+	var cardId = Session.get('cardId');
+	var textInput = $(templ.find("._newnotetitle"));
+	var newnotetitle = textInput.val();
+	if (!newnotetitle) {
+		return;
+	}
+
+	cardColl.update({
+		_id: cardId
+	}, {
+		$push: {
+			notes: {
+				title: newnotetitle
+			}
+		}
+	})
+
+}
+
+Template.noteList.notes = function(){
+
+	var cardId = Session.get('cardId');
+	var ret =  cardColl.findOne({_id : cardId});
+	return ret.notes;
+}
+
+
+Template.noteList.events({
+	'keypress ._newnotetitle': function(e, templ) {
+		if (e.keyCode == 13) {
+					debugger;
+			saveNewNote(e, templ);
+			$(templ.find("._newnotetitle")).blur();
+		}
+	}
+});
